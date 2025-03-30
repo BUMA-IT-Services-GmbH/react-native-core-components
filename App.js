da7256d78b8d82903516494b613edc957b5e30d0
mario.buber <mario.buber@buma-its.com>
2025-03-30 16:13:58 +0200

    add files for CoreComponents

--- App.js
+++ App.js
@@ -1,8 +1,60 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { View, Text, Image, ScrollView, Button, Modal, Alert } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  return <View style={{ flex: 1, backgroundColor: "plum"}}>
-    <View style={{ width: 200, height: 200, backgroundColor: "lightblue" }}></View>
-    <View style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}></View>
-  </View>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          Hello World! This is my first React Native app. I am learning to build mobile applications using React Native.
+          This is an example of a simple app that displays an image and some text. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Button
+          title="Click Me!"
+          onPress={() => {
+            setIsModalVisible(true);
+            Alert.alert("Button clicked!", "You clicked the button!");
+          }}
+          style={{ marginTop: 20 }}
+          color={"midnightblue"}
+        />
+        <Modal
+          visible={isModalVisible}
+          animationType="slide"
+          presentationStyle="pageSheet"
+          onRequestClose={() => {
+            setIsModalVisible(false);
+            alert("Modal closed!");
+          }}
+        >
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0,0,0,0.5)" }}>
+            <View style={{ width: 300, height: 200, backgroundColor: "white", borderRadius: 10, padding: 20 }}>
+              <Text>This is a modal!</Text>
+              <Button
+                title="Close Modal"
+                onPress={() => {
+                  setIsModalVisible(false);
+                  alert("Modal closed!");
+                }}
+                color={"midnightblue"}
+              />
+            </View>
+          </View>
+        </Modal>
+      </ScrollView>
+    </View>
+  );
 }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # react-native-core-components
+
+## Core Components
+
+- View
+- Text
+- Image
+- ImageBackground
+- ScrollView
+- Modal
+- StatusBar
+- ActivityIndicator
+- Alert
+- Custom Components
+
+## Styles
+
+- StyleSheet
+- Box Model
+- Shadow and Elevation
+- Style Inheritance
+
+## Layout
+
+- Flexbox
